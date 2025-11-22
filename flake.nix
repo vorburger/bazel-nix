07814ade3b989dbade0a814deae465d961795ae2
Michael Vorburger <mike@vorburger.ch>
2025-11-22 12:34:29 +0100

    Support macOS darwin (fixes #1)

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     { self, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" ];
       forEachSystem = lib.genAttrs systems;
     in
     {
@@ -21,16 +21,21 @@
             pname = "bazel";
             version = "8.4.2";
 
-            src = pkgs.fetchurl {
+            src = if pkgs.stdenv.isLinux then pkgs.fetchurl {
               url = "https://github.com/bazelbuild/bazel/releases/download/8.4.2/bazel-8.4.2-linux-x86_64";
               sha256 = "sha256-TcjpnfqALiUtrBdtCCAf0VxUKueMRIyKiZdLbzh8KCw=";
-            };
+            } else if pkgs.stdenv.isDarwin then pkgs.fetchurl {
+              url = "https://github.com/bazelbuild/bazel/releases/download/8.4.2/bazel-8.4.2-darwin-x86_64";
+              sha256 = "sha256-znM0YnTDefd4gNuL2LnIVpiF/lbxk4YXN2CUnakHjfA=";
+            } else (
+              builtins.throw "Unsupported system: ${system}"
+            );
 
-            buildInputs = [
+            buildInputs = lib.optionals pkgs.stdenv.isLinux [
               pkgs.stdenv.cc.cc
             ];
 
-            nativeBuildInputs = [
+            nativeBuildInputs = lib.optionals pkgs.stdenv.isLinux [
               pkgs.autoPatchelfHook
             ];
 
@@ -49,7 +54,7 @@
             dontBuild = true;
             dontUnpack = true;
             dontStrip = true;
-            dontPatchELF = false; # !!
+            dontPatchELF = !pkgs.stdenv.isLinux;
           };
 
           # Set this package as the default output, allowing the user to run `nix run .`
