8b59ca13cd51b03fd39251d65d530373a52cf831
Michael Vorburger <mike@vorburger.ch>
2025-11-22 12:45:57 +0100

    feat: Resolved PR feedback

--- flake.nix
+++ flake.nix
@@ -15,21 +15,30 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
+          version = "8.4.2";
+          platforms = {
+            "x86_64-linux" = {
+              systemName = "linux-x86_64";
+              sha256 = "sha256-TcjpnfqALiUtrBdtCCAf0VxUKueMRIyKiZdLbzh8KCw=";
+            };
+            "x86_64-darwin" = {
+              systemName = "darwin-x86_64";
+              sha256 = "sha256-znM0YnTDefd4gNuL2LnIVpiF/lbxk4YXN2CUnakHjfA=";
+            };
+            # TODO aarch64-linux
+            # TODO aarch64-darwin
+          };
+          platform = platforms.${system} or (builtins.throw "Unsupported system: ''${system}");
         in
         {
           bazel-binary = pkgs.stdenv.mkDerivation {
             pname = "bazel";
-            version = "8.4.2";
+            inherit version;
 
-            src = if pkgs.stdenv.isLinux then pkgs.fetchurl {
-              url = "https://github.com/bazelbuild/bazel/releases/download/8.4.2/bazel-8.4.2-linux-x86_64";
-              sha256 = "sha256-TcjpnfqALiUtrBdtCCAf0VxUKueMRIyKiZdLbzh8KCw=";
-            } else if pkgs.stdenv.isDarwin then pkgs.fetchurl {
-              url = "https://github.com/bazelbuild/bazel/releases/download/8.4.2/bazel-8.4.2-darwin-x86_64";
-              sha256 = "sha256-znM0YnTDefd4gNuL2LnIVpiF/lbxk4YXN2CUnakHjfA=";
-            } else (
-              builtins.throw "Unsupported system: ${system}"
-            );
+            src = pkgs.fetchurl {
+              url = "https://github.com/bazelbuild/bazel/releases/download/${version}/bazel-${version}-${platform.systemName}";
+              sha256 = platform.sha256;
+            };
 
             buildInputs = lib.optionals pkgs.stdenv.isLinux [
               pkgs.stdenv.cc.cc
@@ -55,6 +64,14 @@
             dontUnpack = true;
             dontStrip = true;
             dontPatchELF = !pkgs.stdenv.isLinux;
+
+            meta = with lib; {
+              description = "Bazel binary";
+              homepage = "https://github.com/bazelbuild/bazel";
+              license = licenses.asl20;
+              platforms = builtins.attrNames platforms;
+              maintainers = with maintainers; [ vorburger ];
+            };
           };
 
           # Set this package as the default output, allowing the user to run `nix run .`
